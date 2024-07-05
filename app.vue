68a41705310345821485dacd3feb29a2d4bf01d5
Prabh Kang <prabh@hungrypixel.in>
2024-07-05 21:15:52 +0530

    test commit

---
+++ app.vue
@@ -0,0 +1,10 @@
+<script setup>
+const {data, pending} = useFetch('/api/hello');
+</script>
+
+<template>
+  <div>
+    <h3 v-if="pending">Loading data...</h3>
+    <h3 v-else>Data is fetched: {{ data }}</h3>
+  </div>
+</template>
