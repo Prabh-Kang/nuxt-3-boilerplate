d2c56d6e6a75d010391082f73687e9845cab859f
Prabh Kang <prabh@hungrypixel.in>
2024-07-16 00:56:09 +0530

    [ADDED]: CKEditor documentation

--- app.vue
+++ app.vue
@@ -3,8 +3,5 @@ const {data, pending} = useFetch('/api/hello');
 </script>
 
 <template>
-  <div>
-    <h3 v-if="pending">Loading data...</h3>
-    <h3 v-else>Data is fetched: {{ data }}</h3>
-  </div>
+  <MyEditor />
 </template>
---
+++ components/MyEditor.vue
@@ -0,0 +1,38 @@
+<template>
+  <ckeditor :editor="editor" v-model="editorData" :config="editorConfig"></ckeditor>
+</template>
+
+<script>
+import { ClassicEditor, Bold, Essentials, Italic, Mention, Paragraph, Undo, Heading, BlockQuote, Font, Link, List } from 'ckeditor5';
+
+import 'ckeditor5/ckeditor5.css';
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      editor: ClassicEditor,
+      editorData: '<p>Hello from CKEditor 5 in Vue!</p>',
+      editorConfig: {
+        plugins: [Bold, Essentials, Italic, Mention, Paragraph, Undo, Heading, BlockQuote, Font, Link, List],
+        toolbar: ['heading',
+          '|',
+          'bold',
+          'italic',
+          'fontSize',
+          'fontFamily',
+          'fontColor',
+          '|',
+          'link',
+          'bulletedList',
+          'numberedList',
+          'blockQuote'],
+        licenseKey: '<YOUR_LICENSE_KEY>',
+        mention: {
+          // Mention configuration
+        },
+      },
+    };
+  }
+}
+</script>
